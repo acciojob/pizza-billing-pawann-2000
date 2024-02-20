27330cccace12d8a7e034832b77186c7ed0276ed
pawann-2000 <github.3ljwu@ggmaill.aleeas.com>
2024-02-20 12:19:33 +0530

    fixed some code

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,12 +2,12 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private Integer price;
     private Boolean isVeg;
     private String bill;
-    private int cheese;
-    private int toppings;
-    private int takeAway;
+    private Integer cheese;
+    private Integer toppings;
+    private Integer takeAway;
 
 
     public Pizza(Boolean isVeg){
@@ -33,9 +33,20 @@ public class Pizza {
         this.cheese = 80;
     }
 
+    public Integer getCheese(){
+        return this.cheese!=null ? this.cheese : null;
+    }
+
     public void addExtraToppings(){
         // your code goes here
         this.toppings = isVeg ? 70 : 120;
+    }
+
+    public Integer getToppings(){
+        if(this.toppings!=null){
+            return this.toppings;
+        }
+        return 0;
     }
 
     public void addTakeaway(){
@@ -43,12 +54,21 @@ public class Pizza {
         this.takeAway = 20;
     }
 
+    public Integer getTakeaway(){
+        if(this.takeAway!=null){
+            return this.takeAway;
+        }
+        return 0;
+    }
+
     public String getBill(){
         // your code goes here
         this.bill = "Base Price Of The Pizza: "+ this.price + "\n" +
-                "Extra Cheese Added: " + this.cheese + "\n" +
-                "Extra Toppings Added: " + this.toppings + "\n" +
-                (this.takeAway!=0 ? ("Paperbag Added: " + this.takeAway + "\n" + "Total Price: " + (this.price + this.cheese + this.toppings + this.takeAway)) : "Total Price: " + (this.price + this.cheese + this.toppings + this.takeAway)) + "\n";
+                (getCheese()>0 ? "Extra Cheese Added: " + this.cheese + "\n" : "") +
+                (getToppings()>0 ? "Extra Toppings Added: " + this.toppings + "\n" : "") +
+                (getTakeaway()>0 ? "Paperbag Added: " + this.takeAway + "\n" : "") +
+                "Total Price: " + (getPrice() + getCheese() + getToppings() + getTakeaway()) + "\n";
+
         return this.bill;
     }
 }
